1243dfe1f160efc84e924ac1f7901cce5e27a42c
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-10-13 23:57:48 +0100

    added antd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
 
 const App = () => {
     return (
-        <h2>hhhhhh</h2>
+        <div className="app">
+            <div className="navbar">
+
+            </div>
+            <div className="main">
+
+            </div>
+            <div className="footer">
+
+            </div>
+        </div>
     )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Button, Menu, Typography,Avatar} from 'antd'
+import {Link} from 'react-router-dom';
+
+
+
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
