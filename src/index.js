d309d7743fb2c0668ed47d82eb036c50c13bb9c1
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-10-18 16:38:44 +0100

    finish with btn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter as Router } from 'react-router-dom'
 // take the react component and show it on the screen
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Router><App /></Router>, document.getElementById("root"));
