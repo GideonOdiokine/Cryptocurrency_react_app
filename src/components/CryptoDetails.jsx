6725129b4ebcbd823118eb631e63db0198652bc0
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-10-19 17:11:41 +0100

    added index and index

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CryptoDetails = () => {
+    return (
+        <div>
+            ctypD
+        </div>
+    )
+}
+
+export default CryptoDetails
---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cryptocurrencies = () => {
+    return (
+        <div>
+            CyptoCureenc
+        </div>
+    )
+}
+
+export default Cryptocurrencies
