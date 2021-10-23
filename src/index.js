edd51bf82e9232e3cd00f9268fabe5efc4f5a01f
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-10-23 17:20:32 +0100

    import antd/dist/antd.css styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from 'react-router-dom'
-// import 'antd/dist/antd.css'
+import 'antd/dist/antd.css'
 // take the react component and show it on the screen
 ReactDOM.render(<Router><App /></Router>, document.getElementById("root"));
