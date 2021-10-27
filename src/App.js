ad0a137e2e41986a7fce0ff15c39b353dacf0bcc
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-10-27 14:29:47 +0100

    added Home link to footer

--- src/App.js
+++ src/App.js
@@ -40,6 +40,12 @@ const App = () => {
                     Cryptoverse <br />
                     All rights reserved
                 </Typography.Title>
+                <Space>
+                    <Link to='/'>Home</Link>
+                    <Link to='/exchanges'>Exchanges</Link>
+                    <Link to='/news'>News</Link>
+                </Space>
+
             </div>
         </div>
     )
