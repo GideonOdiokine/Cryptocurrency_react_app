08a6891658f811af69bc676271f0c27a6b001af5
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-10-28 22:47:37 +0100

    finished creating my redux store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./app/store";
 import App from "./App";
 import { BrowserRouter as Router } from 'react-router-dom'
 import 'antd/dist/antd.css'
 // take the react component and show it on the screen
-ReactDOM.render(<Router><App /></Router>, document.getElementById("root"));
+ReactDOM.render(<Router>
+    <Provider store={store} >
+        <App />
+    </Provider>
+</Router>, document.getElementById("root"));
