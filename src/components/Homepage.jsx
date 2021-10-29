10dcd41a2ad0806d82980c40c33036952681aa1c
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-10-29 02:31:34 +0100

    passed down data for all coins

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,20 +7,20 @@ import {useGetCryptosQuery} from '../services/cryptoApi'
 
 const {Title} = Typography;
 
-
-
 const Homepage = () => {
     const {data, isFetching} = useGetCryptosQuery()
-    console.log(data)
+    const globalStats = data?.data?.stats
+    console.log(globalStats)
+    if(isFetching) return "Loading......"
     return (
         <>
           <Title level={2} >Global Crypto Stats</Title>
           <Row>
-              <Col span={12} ><Statistic title="Total Cyptocurrencies" value="5" /> </Col>
-              <Col span={12} ><Statistic title="Total Exchanges" value="5" /> </Col>
-              <Col span={12} ><Statistic title="Total Market Cap" value="5" /> </Col>
-              <Col span={12} ><Statistic title="Total 24h Volume" value="5" /> </Col>
-              <Col span={12} ><Statistic title="Total Market" value="5" /> </Col>
+              <Col span={12} ><Statistic title="Total Cyptocurrencies" value={globalStats.total} /> </Col>
+              <Col span={12} ><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /> </Col>
+              <Col span={12} ><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} /> </Col>
+              <Col span={12} ><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /> </Col>
+              <Col span={12} ><Statistic title="Total Market" value={millify(globalStats.totalMarkets)}  /> </Col>
           </Row>
         </>
     )
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
 const cryptoApiHeaders = {
@@ -13,11 +13,11 @@ const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
     baseQuery: fetchBaseQuery({ baseUrl }),
-    endpoints: (builder) => {
+    endpoints: (builder) => ({
         getCryptos: builder.query({
             query: () => createRequest('/coins')
         })
-    }
+    })
 })
 
 export const {
