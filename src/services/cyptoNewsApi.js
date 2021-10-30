6a8c1ffe6d8a92fcf131078ecb1ddc2f5dd3e29c
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-10-30 22:44:56 +0100

    added CryptoNewsApi

--- src/services/cyptoNewsApi.js
+++ src/services/cyptoNewsApi.js
@@ -1,1 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '21b4c9853emshdf9bc0eead1a265p1d71e7jsne7e2aca335d6'
+}
