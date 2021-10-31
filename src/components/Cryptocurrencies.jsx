9257ee374e9ef07286370df40e3bfd4cdf2e29ba
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-11-01 00:33:02 +0100

    added search coins function

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -6,14 +6,18 @@ import {useGetCryptosQuery} from '../services/cryptoApi'
 
 
 const Cryptocurrencies = ({simplified}) => {
-    const count = simplified ? 10 : 100;
-    const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-    const [cryptos, setCryptos] = useState([]);
-   const {searchTerm, setSearchTerm} = useState("")
-
+  //   const count = simplified ? 10 : 100;
+  //   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+  //   const [cryptos, setCryptos] = useState();
+  //  const {searchTerm, setSearchTerm} = useState("")
+   const count = simplified ? 10 : 100;
+   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+   const [cryptos, setCryptos] = useState();
+   const [searchTerm, setSearchTerm] = useState('');
    useEffect(()=>{
+     setCryptos(cryptosList?.data?.coins)
 
-     const filterData = cryptosList?.data?.coins.filter(coin=> coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+     const filterData = cryptosList?.data?.coins.filter(coin=> coin.name.toLowerCase().includes(searchTerm));
 
      setCryptos(filterData)
 
@@ -29,7 +33,7 @@ const Cryptocurrencies = ({simplified}) => {
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
             <Link key={currency.id} to={`/crypto/${currency.id}`}>
-              <Card title={`${currency.rank}. ${currency.name}`} extra={<img className="crypto-image" src={currency.iconUrl} />} hoverable>
+              <Card title={`${currency.rank}. ${currency.name}`} extra={<img className="crypto-image" src={currency.iconUrl} alt="img"/>} hoverable>
                 <p>Price: {millify(currency.price)}</p>
                 <p>Market Cap: {millify(currency.marketCap)}</p>
                 <p>Daily Change: {currency.change}%</p>
