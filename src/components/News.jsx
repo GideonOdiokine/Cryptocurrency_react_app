abc1a299fdc165c711d115456448a4b4cb2d921b
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-11-01 00:27:30 +0100

    imported loader

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment';
+import {useGetCryptoNewsQuery} from '.././services/cyptoNewsApi'
+import Loader from './Loader';
 
-const News = () => {
+const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = ({simplified}) => {
+    const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
+    const {data, isFetching} = useGetCryptoNewsQuery(10);
+    console.log(data)
     return (
         <div>
             News
