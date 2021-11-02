1b2d6930da62786ee957c438e4f5d38e3b7665fe
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-11-02 23:49:12 +0100

    added featured for cryproNewsApi

--- src/services/cyptoNewsApi.js
+++ src/services/cyptoNewsApi.js
@@ -1,20 +1,21 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoNewsApiHeaders = {
-    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
     'x-rapidapi-key': '21b4c9853emshdf9bc0eead1a265p1d71e7jsne7e2aca335d6'
 }
 
-const baseUrl= 'https://bing-news.p.rapidapi.com/'
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com/news'
 
 const createRequest = (url) => ({ url, headers: cryptoNewsApiHeaders })
 
 export const cryptoNewsApi = createApi({
-  reducerPath: 'cryptoNewsApi',
-   baseQuery: fetchBaseQuery({ baseUrl }),
-     endpoints: (builder) => ({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
         getCryptos: builder.query({
-            query: ({newsCategory, count}) => createRequest(`/coins?limit=${count}`)
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
         })
     })
 )}
