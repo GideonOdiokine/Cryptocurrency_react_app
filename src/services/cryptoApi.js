f98a41428ee09e30e07c8128df9e90365d225b3d
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-11-06 06:26:16 +0100

    added cryptoHistoryQuery to endpoints

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -23,9 +23,12 @@ export const cryptoApi = createApi({
         getCryptoDetails: builder.query({
             query: (coinId) => createRequest(`/coin/${coinId}`),
         }),
+        getCryptoHistory: builder.query({
+            query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`),
+        }),
     })
 })
 
 export const {
-    useGetCryptosQuery, useGetExchangesQuery, useGetCryptoDetailsQuery
+    useGetCryptosQuery, useGetExchangesQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery
 } = cryptoApi
