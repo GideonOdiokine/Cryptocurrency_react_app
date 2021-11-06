9f5312d5a8df355486326ec93fead47dd3f31718
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-11-06 04:22:50 +0100

    added width

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -50,7 +50,7 @@ const News = ({ simplified }) => {
 								<Title className='news-title' level={4}>
 									{news.name}
 								</Title>
-								<img style={{maxWidth:'400px', width:"200px",}}
+								<img style={{maxWidth:'400px', width:"200px", maxHeight:'100px'}}
 									src={news?.image?.thumbnail?.contentUrl || demoImage}
 									alt=''
 								/>
